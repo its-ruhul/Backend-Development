87f755413805b111683268f421c6903cad9a97b7
Ruhul <itsruhul@proton.me>
2026-07-21 11:18:38 +0530

    learn middleware and implement error handling

--- Express/server.js
+++ Express/server.js
@@ -1,11 +1,30 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
+import posts from './routes/posts.js';
+import logger from './middleware/logger.js';
+import errorHandler from './middleware/error.js';
+
 const port = process.env.PORT || 8000;
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+//Logger middleware
+app.use(logger);
+
+// Routes
+app.use('/api/posts', posts);
+
+//Error handler
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
 //setup static folder also middleware
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
 
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -14,23 +33,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.get('/about', (req, res) => {
 //   res.sendFile(path.join(__dirname, 'public', 'about.html'));
 // });
-
-let posts = [
-  {id: 1, title: 'Post One'},
-  {id: 2, title: 'Post Two'},
-  {id: 3, title: 'Post Three'},
-  {id: 4, title: 'Post Four'},
-]
-
-app.get('/api/posts', (req, res) => {
-  console.log(req.query);
-
-  res.json(posts);
-})
-
-app.get('/api/posts/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  res.json(posts.filter((post) => post.id === id));
-})
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Backend-Development
 Repository to keep track of my Backend Development Progress
+
+1. Express 31:00
